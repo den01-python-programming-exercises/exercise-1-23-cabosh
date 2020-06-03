151ffd5f470cc451fdd33586be75e0b512bd25da
cabosh <callum.bosher@gmail.com>
2020-06-03 10:51:36 +0100

    Attempted commit

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,15 @@
 def main():
     #write your code below this line
 
+    number = int(input("Give a number: "))
+
+    if number > 0:
+        print("The number is positive.")
+    elif number < 0:
+        print("The number is negative")
+    else:
+        print("Zero!")
+
+
 if __name__ == '__main__':
     main()
